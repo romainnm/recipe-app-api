f7ab3533067d19b06a29c97033e74b12285c6b6c
Romain <insc.romain@gmail.com>
2025-03-03 13:47:50 -0500

    feat: basic test

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """"Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        result = calc.add(1, 2)
+
+        self.assertEqual(result, 3)
